8c2788618a6e877b722d5b8698b6d1f549e95c7d
Kworn <eric13-@hotmail.fr>
2020-12-29 11:41:44 +0100

    MAJ BD

--- docs/ffx-db-init.sql
+++ docs/ffx-db-init.sql
@@ -73,7 +73,7 @@ CREATE TABLE `compte` (
 
 CREATE TABLE `elementaire` (
   `Id_Elementaire` int(11) NOT NULL,
-  `NomElem` varchar(50) DEFAULT NULL
+  `NomElem` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -84,9 +84,9 @@ CREATE TABLE `elementaire` (
 
 CREATE TABLE `item_clef` (
   `Id_Item_Clef` int(11) NOT NULL,
-  `NomItemC` varchar(50) DEFAULT NULL,
-  `DescriptionItemC` varchar(50) DEFAULT NULL,
-  `ImageItemC` varchar(50) DEFAULT NULL,
+  `NomItemC` varchar(255) DEFAULT NULL,
+  `DescriptionItemC` varchar(255) DEFAULT NULL,
+  `ImageItemC` varchar(255) DEFAULT NULL,
   `Id_Location` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -112,15 +112,15 @@ CREATE TABLE `monstre` (
   `Id_Monstre` int(11) NOT NULL,
   `NomM` varchar(50) DEFAULT NULL,
   `HpM` int(11) DEFAULT NULL,
+  `MpM` int(11) DEFAULT NULL,
+  `strength` int(11) DEFAULT NULL,
+  `Magie` int(11) DEFAULT NULL,
   `Accuracy` int(11) DEFAULT NULL,
   `Chance` int(11) DEFAULT NULL,
   `Defence` int(11) DEFAULT NULL,
   `DefenceMagique` int(11) DEFAULT NULL,
   `Agi` varchar(50) DEFAULT NULL,
   `esquive` varchar(50) DEFAULT NULL,
-  `MpM` int(11) DEFAULT NULL,
-  `strength` int(11) DEFAULT NULL,
-  `Magie` int(11) DEFAULT NULL,
   `Overkill` int(11) DEFAULT NULL,
   `ImageM` varchar(255) DEFAULT NULL,
   `Gil` int(11) DEFAULT NULL,
@@ -300,3 +300,31 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--
+-- INSERTION DANS LES TABLES :
+
+-- LOCATION
+INSERT INTO location (NomLoc) VALUES ("Temple de Baaj"), ("Plaine Foudroyée");
+
+-- MONSTRE
+INSERT INTO monstre VALUES (1,"Geosgaeno Boss", 32767,128,36,40,50,15,50,50,48,0,32767,"",0,0), (2,"Aerouge", 200,220,1,16,0,15,1,120,11,13,300,"",144,92);
+
+-- A (JOINTURE MONSTRE/LOCATIN)
+
+INSERT INTO a values (1,1), (2,2);
+
+-- Elementaire
+
+INSERT INTO elementaire VALUES (1, "Feu"), (2,"Glace"), (3,"Foudre"), (4,"Eau");
+
+-- VariantElem
+
+INSERT INTO varianteelem VALUES (1, "Faible"), (2, "Normal"),(3,"Puissante");
+
+-- ItemClef
+
+INSERT INTO item_clef VALUES (1, "Al Bhed Primer I", "Traduit de Y à A", "",1), (2, "Al Bhed Primer II", "Traduit de P à B", "",1), (3, "Al Bhed Primer III", "Traduit de L à C", "",1), (4, "Al Bhed Primer IV", "Traduit de T à D", "",1), (5, "Al Bhed Primer IX", "Traduit de E à I", "",1), (6, "Al Bhed Primer VI", "Traduit de V à F", "",1), (7, "Al Bhed Primer VII", "Traduit de K à G", "",1), (8, "Al Bhed Primer VIII", "Traduit de R à H", "",1), (9, "Al Bhed Primer X", "Traduit de G à K", "",1), (10, "Al Bhed Primer XI", "Traduit de G à K", "",1), (11, "Al Bhed Primer XII", "Traduit de M à L", "",1), (12, "Al Bhed Primer XIV", "Traduit de H à N", "",1), (13, "Al Bhed Primer XIX", "Traduit de C à S", "",1), (14, "Al Bhed Primer XV", "Traduit de U à O", "",1)
+, (15, "Al Bhed Primer XVI", "Traduit de B à P", "",1), (16, "Al Bhed Primer XVII", "Traduit de N à R", "",1), (17, "Al Bhed Primer XVIII", "Traduit de N à R", "",1), (18, "Al Bhed Primer XX", "Traduit de D à T", "",1), (19, "Al Bhed Primer XXI", "Traduit de I à U", "",1)
+, (20, "Al Bhed Primer XXII", "Traduit de J à V", "",1), (21, "Al Bhed Primer XXIII", "Traduit de F à W", "",1), (22, "Al Bhed Primer XXV", "Traduit de O à Y", "",1), (23, "Al Bhed Primer XXVI", "Traduit de T à D", "",1);
+
